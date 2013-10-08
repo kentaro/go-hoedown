e0d380d9ef29da9189a3b2a9e229bad3eef7a4ed
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-08 10:21:14 +0900

    Use `C.strlen`

--- hoedown.go
+++ hoedown.go
@@ -1,6 +1,6 @@
 package hoedown
 
-// #cgo LDFLAGS: -L/Users/kentaro/dev/github/hoedown/hoedown -lhoedown
+// #cgo LDFLAGS: -L/Users/kentaro/.go/src/github.com/kentaro/hoedown/hoedown -lhoedown
 // #include "hoedown.h"
 import "C"
 import "unsafe"
@@ -9,6 +9,5 @@ func Markdown(src string) string {
 	c_str := C.CString(src)
 	defer C.free(unsafe.Pointer(c_str))
 	c_dst := C.markdown_render(c_str)
-	defer C.free(unsafe.Pointer(c_dst))
-	return C.GoStringN(c_dst, 1024) // XXX
+	return C.GoStringN(c_dst, C.int(C.strlen(c_dst)))
 }
